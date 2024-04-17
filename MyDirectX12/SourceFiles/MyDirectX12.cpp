6ff94bb56007a381ae727c2037fb98651cc68154
MichaelYang <michaeljamestd@hotmail.com>
2024-04-17 15:06:55 +0800

    ## Format code

--- MyDirectX12/SourceFiles/MyDirectX12.cpp
+++ MyDirectX12/SourceFiles/MyDirectX12.cpp
@@ -1,5 +1,4 @@
 // MyDirectX12.cpp : Defines the entry point for the application.
-//
 
 #include "framework.h"
 #include "MyDirectX12.h"
@@ -17,10 +16,7 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-    _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPWSTR    lpCmdLine,
-    _In_ int       nCmdShow)
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR    lpCmdLine, _In_ int       nCmdShow)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
@@ -53,13 +49,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     return (int)msg.wParam;
 }
 
-
-
+// FUNCTION: MyRegisterClass()
 //
-//  FUNCTION: MyRegisterClass()
-//
-//  PURPOSE: Registers the window class.
-//
+// PURPOSE: Registers the window class.
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
     WNDCLASSEXW wcex;
@@ -81,16 +73,13 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     return RegisterClassExW(&wcex);
 }
 
+// FUNCTION: InitInstance(HINSTANCE, int)
 //
-//   FUNCTION: InitInstance(HINSTANCE, int)
+// PURPOSE: Saves instance handle and creates main window
 //
-//   PURPOSE: Saves instance handle and creates main window
+// COMMENTS:
 //
-//   COMMENTS:
-//
-//        In this function, we save the instance handle in a global variable and
-//        create and display the main program window.
-//
+// In this function, we save the instance handle in a global variable and create and display the main program window.
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
     hInst = hInstance; // Store instance handle in our global variable
@@ -109,70 +98,72 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
     return TRUE;
 }
 
+// FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+// PURPOSE: Processes messages for the main window.
 //
-//  PURPOSE: Processes messages for the main window.
-//
-//  WM_COMMAND  - process the application menu
-//  WM_PAINT    - Paint the main window
-//  WM_DESTROY  - post a quit message and return
-//
-//
+// WM_COMMAND - process the application menu WM_PAINT - Paint the main window WM_DESTROY - post a quit message and return
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
-    case WM_COMMAND:
-    {
-        int wmId = LOWORD(wParam);
-        // Parse the menu selections:
-        switch (wmId)
+        case WM_COMMAND:
         {
-        case IDM_ABOUT:
-            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            int wmId = LOWORD(wParam);
+            // Parse the menu selections:
+            switch (wmId)
+            {
+                case IDM_ABOUT:
+                    DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                    break;
+                case IDM_EXIT:
+                    DestroyWindow(hWnd);
+                    break;
+                default:
+                    return DefWindowProc(hWnd, message, wParam, lParam);
+            }
             break;
-        case IDM_EXIT:
-            DestroyWindow(hWnd);
+        }
+        case WM_PAINT:
+        {
+            PAINTSTRUCT _ps;
+            HDC _hdc = BeginPaint(hWnd, &_ps);
+            // TODO: Add any drawing code that uses hdc here...
+            EndPaint(hWnd, &_ps);
             break;
+        }
+        case WM_DESTROY:
+        {
+            PostQuitMessage(0);
+            break;
+        }
         default:
+        {
             return DefWindowProc(hWnd, message, wParam, lParam);
         }
-    }
-    break;
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hWnd, &ps);
-        // TODO: Add any drawing code that uses hdc here...
-        EndPaint(hWnd, &ps);
-    }
-    break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
 }
 
 // Message handler for about box.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+INT_PTR CALLBACK About(HWND hDialog, UINT message, WPARAM wParam, LPARAM lParam)
 {
     UNREFERENCED_PARAMETER(lParam);
     switch (message)
     {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        case WM_INITDIALOG:
         {
-            EndDialog(hDlg, LOWORD(wParam));
             return (INT_PTR)TRUE;
         }
-        break;
+        case WM_COMMAND:
+        {
+            if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+            {
+                EndDialog(hDialog, LOWORD(wParam));
+                return (INT_PTR)TRUE;
+                break;
+            }
+        }
     }
     return (INT_PTR)FALSE;
 }
